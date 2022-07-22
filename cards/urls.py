c4dc04da427cf514a13a7def66eb6648d977e2ca
Mike <mikestewart729@gmail.com>
2022-07-22 16:20:28 -0400

    Allow card creation/updates with better navigation

--- cards/urls.py
+++ cards/urls.py
@@ -9,4 +9,14 @@ urlpatterns = [
         views.CardListView.as_view(),
         name="card-list"
     ),
+    path(
+        "new",
+        views.CardCreateView.as_view(),
+        name="card-create"
+    ),
+    path(
+        "edit/<int:pk>",
+        views.CardUpdateView.as_view(),
+        name="card-update"
+    ),
 ]
